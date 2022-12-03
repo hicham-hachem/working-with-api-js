4ae9865b0de50e5d5e8ba8390de164d019de53bf
hicham <hicham.hachem1@gmail.com>
2022-12-03 23:04:40 +0200

    Image loading

--- script.js
+++ script.js
@@ -241,3 +241,75 @@ const whereAmI2 = function () {
 };
 
 btn.addEventListener('click', whereAmI2);
+
+//////////////////////////////////////////////////////////////////////
+
+// Image loading
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+    return new Promise(function (resolve, reject) {
+        const img = document.createElement('img');
+        img.src = imgPath;
+
+        img.addEventListener('load', function () {
+            imgContainer.append(img);
+            resolve(img);
+        });
+
+        img.addEventListener('error', function () {
+            reject(new Error('Image not found'));
+        });
+    });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+    // Load image 1
+    .then(img => {
+        currentImg = img;
+        console.log('Image 1 loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+        return createImage('img/img-2.jpg');
+    })
+    // Load image 2
+    .then(img => {
+        currentImg = img;
+        console.log('Image 2 loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+        return createImage('img/img-3.jpg');
+    })
+    // Load image 3
+    .then(img => {
+        currentImg = img;
+        console.log('Image 3 loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+        return createImage('img/img-4.jpg');
+    })
+    // Load image 4 (There is no image 4)
+    .then(img => {
+        currentImg = img;
+        console.log('Image 4 loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+    })
+
+    .catch(err => console.error(err));
